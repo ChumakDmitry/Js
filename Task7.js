267163f802dda1647726ccb6ca2928af9c1d6db4
Dmitriy Chumak <chumak.exceedteam@gmail.com>
2022-09-28 13:46:43 +0300

    fix problem

--- Task7.js
+++ Task7.js
@@ -8,20 +8,18 @@ const checkPalyndrome = (str) => {
     return "error";
   }
 
-  str = str.toLowerCase().split('');
+  let resultString = str.toLowerCase();
 
-  for (let i = 0 ; i < str.length; i++) {
-    if (str[i] === ',' || str[i] === '-' || str[i] === '.' || str[i] === ' ') {
-      str.splice(i, 1);
+  for (let i = 0 ; i < resultString.length; i++) {
+    if (resultString[i] === ',' || resultString[i] === '-' || resultString[i] === '.' || resultString[i] === ' ') {
+      resultString.slice(i, 1);
       i--;
     }
   }
-
-  str = str.join('');
   let flag = true;
 
   for (let i = 0, j = str.length - 1; i < str.length / 2; i++, j--) {
-    if (str[i] !== str[j]) {
+    if (resultString[i] !== resultString[j]) {
       return flag = false;
     }
   }
